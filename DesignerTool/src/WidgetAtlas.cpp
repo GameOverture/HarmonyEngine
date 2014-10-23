115e314c8838dead664aa5f0f7b119aeda14f4a2
Jason Knobler <jayknobler@gmail.com>
2014-10-22 23:29:50 -0400

    Framing out the Atlas widget

--- DesignerTool/src/WidgetAtlas.cpp
+++ DesignerTool/src/WidgetAtlas.cpp
@@ -12,3 +12,74 @@ WidgetAtlas::~WidgetAtlas()
 {
     delete ui;
 }
+
+void WidgetAtlas::on_btnTexSize256_clicked()
+{
+    ui->sbTextureWidth->setValue(256);
+    ui->sbTextureHeight->setValue(256);
+}
+
+void WidgetAtlas::on_btnTexSize512_clicked()
+{
+    ui->sbTextureWidth->setValue(512);
+    ui->sbTextureHeight->setValue(512);
+}
+
+void WidgetAtlas::on_btnTexSize1024_clicked()
+{
+    ui->sbTextureWidth->setValue(1024);
+    ui->sbTextureHeight->setValue(1024);
+}
+
+void WidgetAtlas::on_btnTexSize2048_clicked()
+{
+    ui->sbTextureWidth->setValue(2048);
+    ui->sbTextureHeight->setValue(2048);
+}
+
+void WidgetAtlas::on_btnAddFiles_clicked()
+{
+    
+}
+
+void WidgetAtlas::on_btnAddDir_clicked()
+{
+    
+}
+
+void WidgetAtlas::on_cmbHeuristic_currentIndexChanged(const QString &arg1)
+{
+    
+}
+
+
+void WidgetAtlas::on_cmbSortOrder_currentIndexChanged(const QString &arg1)
+{
+    
+}
+
+void WidgetAtlas::on_cmbRotationStrategy_currentIndexChanged(const QString &arg1)
+{
+    
+}
+
+
+void WidgetAtlas::on_sbFrameMarginTop_valueChanged(int arg1)
+{
+    
+}
+
+void WidgetAtlas::on_sbFrameMarginRight_valueChanged(int arg1)
+{
+    
+}
+
+void WidgetAtlas::on_sbFrameMarginBottom_valueChanged(int arg1)
+{
+    
+}
+
+void WidgetAtlas::on_sbFrameMarginLeft_valueChanged(int arg1)
+{
+    
+}
--- DesignerTool/src/WidgetAtlas.h
+++ DesignerTool/src/WidgetAtlas.h
@@ -3,6 +3,8 @@
 
 #include <QWidget>
 
+#include "scriptum/imagepacker.h"
+
 namespace Ui {
 class WidgetAtlas;
 }
@@ -11,9 +13,38 @@ class WidgetAtlas : public QWidget
 {
     Q_OBJECT
     
+    ImagePacker         m_packer;
+    
 public:
     explicit WidgetAtlas(QWidget *parent = 0);
     ~WidgetAtlas();
+    
+private slots:
+    void on_btnTexSize256_clicked();
+    
+    void on_btnTexSize512_clicked();
+    
+    void on_btnTexSize1024_clicked();
+    
+    void on_btnTexSize2048_clicked();
+    
+    void on_btnAddFiles_clicked();
+    
+    void on_btnAddDir_clicked();
+    
+    void on_cmbHeuristic_currentIndexChanged(const QString &arg1);
+    
+    void on_cmbSortOrder_currentIndexChanged(const QString &arg1);
+    
+    void on_cmbRotationStrategy_currentIndexChanged(const QString &arg1);
+    
+    void on_sbFrameMarginTop_valueChanged(int arg1);
+    
+    void on_sbFrameMarginRight_valueChanged(int arg1);
+    
+    void on_sbFrameMarginBottom_valueChanged(int arg1);
+    
+    void on_sbFrameMarginLeft_valueChanged(int arg1);
     
 private:
     Ui::WidgetAtlas *ui;
