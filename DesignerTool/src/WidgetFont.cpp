5506b6ab0703859ec67c66858ad4adb126704b2d
OvertureGames <jayknobler@gmail.com>
2016-10-10 11:01:35 -0400

    New font will add a normal Arial Layer to start

--- DesignerTool/src/WidgetFont.cpp
+++ DesignerTool/src/WidgetFont.cpp
@@ -98,7 +98,9 @@ WidgetFont::WidgetFont(ItemFont *pOwner, QWidget *parent) : QWidget(parent),
     else
     {
         ui->cmbAtlasGroups->setCurrentIndex(m_pItemFont->GetAtlasManager().CurrentAtlasGroupIndex());
+        
         on_actionAddState_triggered();
+        on_actionAddLayer_triggered();
     }
     
     // Clear the UndoStack because we don't want any of the above initialization to be able to be undone.
@@ -111,12 +113,8 @@ WidgetFont::WidgetFont(ItemFont *pOwner, QWidget *parent) : QWidget(parent),
 
 WidgetFont::~WidgetFont()
 {
-//    for(int i = 0; i < m_MasterStageList.count(); ++i)
-//        delete m_MasterStageList[i];
-
-//    QMap<int, FontStage *>::iterator iter;
-//    for(iter = m_RemovedStageMap.begin(); iter != m_RemovedStageMap.end(); ++iter)
-//        delete iter.value();
+    for(int i = 0; i < m_MasterStageList.count(); ++i)
+        delete m_MasterStageList[i];
 
     delete ui;
 }
@@ -130,85 +128,6 @@ QString WidgetFont::GetFullItemName()
 {
     return m_pItemFont->GetName(true);
 }
-
-//// This changes the document state, and should only be invoked through 'ItemFontCmd_*'
-//int WidgetFont::AddNewStage(QString sFullFontPath, rendermode_t eRenderMode, float fSize, float fOutlineThickness)
-//{
-//    // Look for an existing stage that matches the request first
-//    FontStagePass *pStage = NULL;
-//    for(int i = 0; i < m_MasterStageList.count(); ++i)
-//    {
-//        pStage = m_MasterStageList[i];
-
-//        QFileInfo stageFontPath(pStage->pTextureFont->filename);
-//        QFileInfo requestFontPath(sFullFontPath);
-
-//        if(QString::compare(stageFontPath.fileName(), requestFontPath.fileName(), Qt::CaseInsensitive) == 0 &&
-//           pStage->eMode == eRenderMode &&
-//           pStage->fSize == fSize &&
-//           pStage->fOutlineThickness == fOutlineThickness)
-//        {
-//            ui->cmbStates->currentData().value<WidgetFontState *>()->GetFontModel()->AddStage(pStage);
-//            return pStage->iUNIQUE_ID;
-//        }
-//    }
-
-//    sm_iUniqueIdCounter++;
-
-//    pStage = new FontStage(sm_iUniqueIdCounter, eRenderMode, fSize, fOutlineThickness);
-//    ui->cmbStates->currentData().value<WidgetFontState *>()->GetFontModel()->AddStage(pStage);
-//    m_MasterStageList.append(pStage);
-
-//    return pStage->iUNIQUE_ID;
-//}
-
-//// This changes the document state, and should only be invoked through 'ItemFontCmd_*'
-//void WidgetFont::RemoveStage(int iId)
-//{
-//    for(int i = 0; i < m_MasterStageList.count(); ++i)
-//    {
-//        if(m_MasterStageList[i]->iUNIQUE_ID == iId)
-//        {
-//            ui->cmbStates->currentData().value<WidgetFontState *>()->GetFontModel()->RemoveStage(m_MasterStageList[i]);
-//            return;
-//        }
-//    }
-//}
-
-//// This changes the document state, and should only be invoked through 'ItemFontCmd_*'
-//void WidgetFont::ReAddStage(int iId)
-//{
-//    for(int i = 0; i < m_MasterStageList.count(); ++i)
-//    {
-//        if(m_MasterStageList[i]->iUNIQUE_ID == iId)
-//        {
-//            ui->cmbStates->currentData().value<WidgetFontState *>()->GetFontModel()->AddStage(m_MasterStageList[i]);
-//            return;
-//        }
-//    }
-//}
-
-//void WidgetFont::SetStageRenderMode(WidgetFontState *pFontState, int iStageId, rendermode_t eRenderMode)
-//{
-//    for(int i = 0; i < m_MasterStageList.count(); ++i)
-//    {
-//        if(m_MasterStageList[i]->iUNIQUE_ID == iStageId)
-//        {
-//            // FontStage found, but now ensure it isn't used elsewhere. If it is, then create a new font stage with the same properties plus this change
-//            for(int j = 0; j < ui->cmbStates->count(); ++j)
-//            {
-//                WidgetFontState *pFontState = ui->cmbStates->itemData(j).value<WidgetFontState *>();
-                
-//                if(pFontState->GetFontModel() == pFontModel)
-//                {
-                    
-//                }
-//            }
-
-//            return;
-//        }
-//    }
-//}
 
 void WidgetFont::GeneratePreview(bool bFindBestFit /*= false*/)
 {
