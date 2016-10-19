f9e4e8a7e2a7acade86b1e47dbc56ae56d66bb80
OvertureGames <jayknobler@gmail.com>
2016-10-19 17:45:40 -0400

    Started HyText2dData loading. WIP

--- Harmony/include/Harmony/Assets/Data/HyText2dData.h
+++ Harmony/include/Harmony/Assets/Data/HyText2dData.h
@@ -24,6 +24,63 @@ class HyText2dData : public IHy2dData
 {
 	friend class HyFactory<HyText2dData>;
 
+	struct GlyphInfo
+	{
+		const uint32				uiWIDTH;
+		const uint32				uiHEIGHT;
+
+		const int					iOFFSET_X;
+		const int					iOFFSET_Y;
+
+		const float					fADVANCE_X;
+		const float					fADVANCE_Y;
+
+		const HyRectangle<float>	rSRC_RECT;
+
+		std::map<uint32, float>		kerningMap;
+
+		GlyphInfo(uint32 uiWidth,
+				  uint32 uiHeight,
+				  int iOffsetX,
+				  int iOffsetY,
+				  float fAdvanceX,
+				  float fAdvanceY,
+				  float fSrcLeft,
+				  float fSrcTop,
+				  float fSrcRight,
+				  float fSrcBot,
+				  jsonxx::Object &kerningObj) :	uiWIDTH(uiWidth),
+												uiHEIGHT(uiHeight),
+												iOFFSET_X(iOffsetX),
+												iOFFSET_Y(iOffsetY),
+												fADVANCE_X(fAdvanceX),
+												fADVANCE_Y(fAdvanceY),
+												rSRC_RECT(fSrcLeft, fSrcTop, fSrcRight, fSrcBot)
+		{
+			kerningMap;
+		}
+	};
+	typedef std::map<uint32, GlyphInfo> Typeface;
+
+	Typeface *						m_pTypefaces;
+	uint32							m_uiNumTypefaces;
+
+	struct FontState
+	{
+		const std::string	m_sNAME;
+
+		struct Layer
+		{
+			const glm::vec4			m_vDEFAULT_TOP_COLOR;
+			const glm::vec4			m_vDEFAULT_BOT_COLOR;
+			const Typeface &		m_TYPEFACE_REF;
+		};
+		Layer *						m_pLayers;
+		uint32						m_uiNumLayers;
+	};
+	FontState *						m_pFontStates;
+	uint32							m_uiNumStates;
+
 	// Only allow HyFactory instantiate
 	HyText2dData(const std::string &sPath, int32 iShaderId);
 
--- Harmony/src/Assets/Data/HyText2dData.cpp
+++ Harmony/src/Assets/Data/HyText2dData.cpp
@@ -21,5 +21,28 @@ HyText2dData::~HyText2dData(void)
 
 /*virtual*/ void HyText2dData::DoFileLoad()
 {
+	std::string sFontFileContents;
+	HyReadTextFile(GetPath().c_str(), sFontFileContents);
 
+	jsonxx::Object textObject;
+	textObject.parse(sFontFileContents);
+
+	jsonxx::Array stateArray = textObject.get<jsonxx::Array>("stateArray");
+	//for(int i = 0; i < 
+
+	//m_uiNumStates = static_cast<uint32>(spriteStateArray.size());
+	//m_pAnimStates = reinterpret_cast<AnimState *>(HY_NEW unsigned char[sizeof(AnimState) * m_uiNumStates]);
+	//AnimState *pAnimStateWriteLocation = m_pAnimStates;
+
+	//for(uint32 i = 0; i < m_uiNumStates; ++i, ++pAnimStateWriteLocation)
+	//{
+	//	jsonxx::Object spriteStateObj = spriteStateArray.get<jsonxx::Object>(i);
+
+	//	new (pAnimStateWriteLocation)AnimState(spriteStateObj.get<jsonxx::String>("name"),
+	//		spriteStateObj.get<jsonxx::Boolean>("loop"),
+	//		spriteStateObj.get<jsonxx::Boolean>("reverse"),
+	//		spriteStateObj.get<jsonxx::Boolean>("bounce"),
+	//		spriteStateObj.get<jsonxx::Array>("frames"),
+	//		*this);
+	//}
 }
