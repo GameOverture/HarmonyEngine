35a91d158405a1385ee0258332b4d6f61255fed5
OvertureGames <jayknobler@gmail.com>
2016-11-16 15:58:20 -0500

    WidgetRangedSlider is feature complete

--- DesignerTool/src/WidgetRangedSlider.cpp
+++ DesignerTool/src/WidgetRangedSlider.cpp
@@ -16,11 +16,19 @@ WidgetRangedSlider::WidgetRangedSlider(QWidget *parent) :   QWidget(parent),
                                                             ui(new Ui::WidgetRangedSlider)
 {
     ui->setupUi(this);
+    
+    SetRange(0, 100);
+    SetValue(40, 60);
 }
 
 WidgetRangedSlider::~WidgetRangedSlider()
 {
     delete ui;
+}
+
+bool WidgetRangedSlider::IsIntType()
+{
+    return ui->stackedSpinBoxes->currentIndex() == TYPE_INT;
 }
 
 QString WidgetRangedSlider::GetTitle()
@@ -42,6 +50,8 @@ void WidgetRangedSlider::SetRange(int iMin, int iMax)
     
     ui->sbMin_Int->setRange(iMin, iMax);
     ui->sbMax_Int->setRange(iMin, iMax);
+    
+    OnDataCorrect();
 }
 
 void WidgetRangedSlider::SetRange(double dMin, double dMax, int iNumDecimalPrecision)
@@ -71,6 +81,8 @@ void WidgetRangedSlider::SetRange(double dMin, double dMax, int iNumDecimalPreci
     
     ui->minSlider->setRange(iSliderMin, iSliderMax);
     ui->maxSlider->setRange(iSliderMin, iSliderMax);
+    
+    OnDataCorrect();
 }
 
 void WidgetRangedSlider::SetValue(int iMin, int iMax)
@@ -81,8 +93,10 @@ void WidgetRangedSlider::SetValue(int iMin, int iMax)
         return;
     }
 
-    ui->minSlider->blockSignals(true);
-
+    ui->sbMin_Int->setValue(iMin);
+    ui->sbMax_Int->setValue(iMax);
+    
+    OnDataCorrect();
 }
 
 void WidgetRangedSlider::SetValue(double dMin, double dMax)
@@ -92,13 +106,183 @@ void WidgetRangedSlider::SetValue(double dMin, double dMax)
         SetValue(static_cast<int>(dMin), static_cast<int>(dMax));
         return;
     }
+    
+    ui->sbMin_Double->setValue(dMin);
+    ui->sbMax_Double->setValue(dMax);
+    
+    OnDataCorrect();
 }
 
 QVariant WidgetRangedSlider::GetMin()
 {
+    if(ui->stackedSpinBoxes->currentIndex() == TYPE_INT)
+        return QVariant(ui->sbMin_Int->value());
+    else
+        return QVariant(ui->sbMin_Double->value());
 }
 
 QVariant WidgetRangedSlider::GetMax()
 {
+    if(ui->stackedSpinBoxes->currentIndex() == TYPE_INT)
+        return QVariant(ui->sbMax_Int->value());
+    else
+        return QVariant(ui->sbMax_Double->value());
 }
 
+void WidgetRangedSlider::OnDataCorrect()
+{
+    if(ui->stackedSpinBoxes->currentIndex() == TYPE_INT)
+    {
+        if(ui->sbMin_Int->value() > ui->sbMax_Int->value())
+            ui->sbMin_Int->setValue(ui->sbMax_Int->value());
+        else if(ui->sbMax_Int->value() < ui->sbMin_Int->value())
+            ui->sbMax_Int->setValue(ui->sbMin_Int->value());
+
+        ui->minSlider->setValue(ui->sbMin_Int->value());
+        ui->maxSlider->setValue(ui->sbMax_Int->value());
+        
+        m_iPrevMin = ui->sbMin_Int->value();
+        m_iPrevMax = ui->sbMax_Int->value();
+    }
+    else
+    {
+        if(ui->sbMin_Double->value() > ui->sbMax_Double->value())
+            ui->sbMin_Double->setValue(ui->sbMax_Double->value());
+        else if(ui->sbMax_Double->value() < ui->sbMin_Double->value())
+            ui->sbMax_Double->setValue(ui->sbMin_Double->value());
+        
+        double dMinValue = ui->sbMin_Double->value();
+        for(int i = 0; i < ui->sbMin_Double->decimals(); ++i)
+            dMinValue *= 10.0f;
+        
+        double dMaxValue = ui->sbMax_Double->value();
+        for(int i = 0; i < ui->sbMax_Double->decimals(); ++i)
+            dMaxValue *= 10.0f;
+
+        ui->minSlider->setValue(static_cast<int>(dMinValue));
+        ui->maxSlider->setValue(static_cast<int>(dMaxValue));
+        
+        m_dPrevMin = ui->sbMin_Double->value();
+        m_dPrevMax = ui->sbMax_Double->value();
+    }
+}
+
+void WidgetRangedSlider::on_sbMin_Double_editingFinished()
+{
+    QVariant oldMin, oldMax, newMin, newMax;
+    
+    oldMin = QVariant(m_dPrevMin);
+    oldMax = QVariant(m_dPrevMax);
+    OnDataCorrect();
+    newMin = QVariant(ui->sbMin_Double->value());
+    newMax = QVariant(ui->sbMax_Double->value());
+    
+    emit userChangedValue(oldMin, oldMax, newMin, newMax);
+}
+
+void WidgetRangedSlider::on_sbMax_Double_editingFinished()
+{
+    QVariant oldMin, oldMax, newMin, newMax;
+    
+    oldMin = QVariant(m_dPrevMin);
+    oldMax = QVariant(m_dPrevMax);
+    OnDataCorrect();
+    newMin = QVariant(ui->sbMin_Double->value());
+    newMax = QVariant(ui->sbMax_Double->value());
+    
+    emit userChangedValue(oldMin, oldMax, newMin, newMax);
+}
+
+void WidgetRangedSlider::on_sbMin_Int_editingFinished()
+{
+    QVariant oldMin, oldMax, newMin, newMax;
+    
+    oldMin = QVariant(m_iPrevMin);
+    oldMax = QVariant(m_iPrevMax);
+    OnDataCorrect();
+    newMin = QVariant(ui->sbMin_Int->value());
+    newMax = QVariant(ui->sbMax_Int->value());
+    
+    emit userChangedValue(oldMin, oldMax, newMin, newMax);
+}
+
+void WidgetRangedSlider::on_sbMax_Int_editingFinished()
+{
+    QVariant oldMin, oldMax, newMin, newMax;
+    
+    oldMin = QVariant(m_iPrevMin);
+    oldMax = QVariant(m_iPrevMax);
+    OnDataCorrect();
+    newMin = QVariant(ui->sbMin_Int->value());
+    newMax = QVariant(ui->sbMax_Int->value());
+    
+    emit userChangedValue(oldMin, oldMax, newMin, newMax);
+}
+
+void WidgetRangedSlider::on_maxSlider_sliderMoved(int position)
+{
+    QVariant oldMin, oldMax, newMin, newMax;
+    
+    if(ui->stackedSpinBoxes->currentIndex() == TYPE_INT)
+    {
+        oldMin = QVariant(m_iPrevMin);
+        oldMax = QVariant(m_iPrevMax);
+        
+        ui->sbMax_Int->setValue(ui->maxSlider->value());
+        OnDataCorrect();
+        
+        newMin = QVariant(ui->sbMin_Int->value());
+        newMax = QVariant(ui->sbMax_Int->value());
+    }
+    else
+    {
+        oldMin = QVariant(m_dPrevMin);
+        oldMax = QVariant(m_dPrevMax);
+        
+        double dValue = ui->maxSlider->value();
+        for(int i = 0; i < ui->sbMax_Double->decimals(); ++i)
+            dValue /= 10.0f;
+        
+        ui->sbMax_Double->setValue(dValue);
+        OnDataCorrect();
+        
+        newMin = QVariant(ui->sbMin_Double->value());
+        newMax = QVariant(ui->sbMax_Double->value());
+    }
+    
+    emit userChangedValue(oldMin, oldMax, newMin, newMax);
+}
+
+void WidgetRangedSlider::on_minSlider_sliderMoved(int position)
+{
+    QVariant oldMin, oldMax, newMin, newMax;
+    
+    if(ui->stackedSpinBoxes->currentIndex() == TYPE_INT)
+    {
+        oldMin = QVariant(m_iPrevMin);
+        oldMax = QVariant(m_iPrevMax);
+        
+        ui->sbMin_Int->setValue(ui->minSlider->value());
+        OnDataCorrect();
+        
+        newMin = QVariant(ui->sbMin_Int->value());
+        newMax = QVariant(ui->sbMax_Int->value());
+    }
+    else
+    {
+        oldMin = QVariant(m_dPrevMin);
+        oldMax = QVariant(m_dPrevMax);
+        
+        double dValue = ui->minSlider->value();
+        for(int i = 0; i < ui->sbMin_Double->decimals(); ++i)
+            dValue /= 10.0f;
+        
+        ui->sbMin_Double->setValue(dValue);
+        OnDataCorrect();
+        
+        newMin = QVariant(ui->sbMin_Double->value());
+        newMax = QVariant(ui->sbMax_Double->value());
+    }
+    
+    emit userChangedValue(oldMin, oldMax, newMin, newMax);
+}
--- DesignerTool/src/WidgetRangedSlider.h
+++ DesignerTool/src/WidgetRangedSlider.h
@@ -26,9 +26,16 @@ class WidgetRangedSlider : public QWidget
         TYPE_DOUBLE
     };
     
+    int                         m_iPrevMin;
+    int                         m_iPrevMax;
+    double                      m_dPrevMin;
+    double                      m_dPrevMax;
+    
 public:
     explicit WidgetRangedSlider(QWidget *parent = 0);
     ~WidgetRangedSlider();
+    
+    bool IsIntType();
     
     QString GetTitle();
     void SetTitle(QString sTitle);
@@ -42,8 +49,26 @@ public:
     QVariant GetMin();
     QVariant GetMax();
     
+signals:
+    void userChangedValue(QVariant oldMin, QVariant oldMax, QVariant newMin, QVariant newMax);
+    
+private slots:
+    void on_sbMin_Double_editingFinished();
+    
+    void on_sbMax_Double_editingFinished();
+    
+    void on_sbMin_Int_editingFinished();
+    
+    void on_sbMax_Int_editingFinished();
+    
+    void on_maxSlider_sliderMoved(int position);
+    
+    void on_minSlider_sliderMoved(int position);
+    
 private:
     Ui::WidgetRangedSlider *ui;
+    
+    void OnDataCorrect();
 };
 
 #endif // WIDGETRANGEDSLIDER_H
