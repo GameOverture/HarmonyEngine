0385f728886a1a87c9d5cdfa63020366f55daf25
Jason Knobler <jayknobler@gmail.com>
2018-07-12 01:17:58 -0400

    Prefab loading of 3d asset WIP

--- Harmony/src/Editor/Prefab/PrefabModel.cpp
+++ Harmony/src/Editor/Prefab/PrefabModel.cpp
@@ -21,9 +21,73 @@ PrefabModel::PrefabModel(ProjectItem &itemRef, QJsonValue initValue) : IModel(it
 	{
 		Assimp::Importer importer;
 		const aiScene *pScene = importer.ReadFile(initValue.toString().toStdString(), aiProcess_CalcTangentSpace | aiProcess_Triangulate | aiProcess_JoinIdenticalVertices | aiProcess_SortByPType);
-		pScene->HasMeshes();
+		if(pScene->mFlags & AI_SCENE_FLAGS_INCOMPLETE)
+		{
+			HyGuiLog(initValue.toString() + " was incomplete and is invalid", LOGTYPE_Error);
+			return;
+		}
+
+		//  http://ogldev.atspace.co.uk/index.html
+
+		m_MeshList.reserve(pScene->mNumMeshes);
+		m_sTextureList.reserve(pScene->mNumMaterials);
+
+		// Rip all required textures from this prefab
+		if(pScene->HasTextures())
+		{
+		}
+
+		// GetTexture Filenames and Numb of Textures
+		for(uint i = 0; i < pScene->mNumMaterials; ++i)
+		{
+			uint uiTexIndex = 0;
+			aiReturn texFound = AI_SUCCESS;
+			aiString sFilePath;
+			while(true)
+			{
+				texFound = pScene->mMaterials[i]->GetTexture(aiTextureType_DIFFUSE, uiTexIndex, &sFilePath);
+				if(texFound == AI_SUCCESS)
+				{
+					m_sTextureDiffuseList.append(sFilePath.data);
+					uiTexIndex++;
+				}
+				else
+					break;
+			}
+		}
+
+		//int iNumTextures = m_sTextureFilePathList.size();
+		//iNumTextures = iNumTextures;
 	}
 }
+
+//void PrefabModel::CopyNodesWithMeshes( aiNode node, SceneObject targetParent, Matrix4x4 accTransform)
+//{
+//	SceneObject parent;
+//	Matrix4x4 transform;
+
+//	// if node has meshes, create a new scene object for it
+//	if(node.mNumMeshes > 0)
+//	{
+//		SceneObjekt newObject = new SceneObject;
+//		targetParent.addChild( newObject);
+//		// copy the meshes
+//		CopyMeshes( node, newObject);
+//		// the new object is the parent for all child nodes
+//		parent = newObject;
+//		transform.SetUnity();
+//	}
+//	else
+//	{
+//		// if no meshes, skip the node, but keep its transformation
+//		parent = targetParent;
+//		transform = node.mTransformation * accTransform;
+//	}
+
+//	// continue for all child nodes
+//	for( all node.mChildren)
+//	CopyNodesWithMeshes( node.mChildren[a], parent, transform);
+//}
 
 /*virtual*/ void PrefabModel::OnSave() /*override*/
 {
