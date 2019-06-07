1e82ded51de245f358d37418fdca01aaaaeb4d16
Jason Knobler <jayknobler@gmail.com>
2019-06-06 20:21:56 -0400

    Added Audio harness class

---
+++ Harmony/include/Harmony/Audio/HyAudioHarness.h
@@ -0,0 +1,16 @@
+/**************************************************************************
+*	HyAudioHarness.h
+*
+*	Harmony Engine
+*	Copyright (c) 2019 Jason Knobler
+*
+*	Harmony License:
+*	https://github.com/OvertureGames/HarmonyEngine/blob/master/LICENSE
+*************************************************************************/
+class HyAudioHarness
+{
+public:
+	HyAudioHarness();
+	~HyAudioHarness();
+};
+
