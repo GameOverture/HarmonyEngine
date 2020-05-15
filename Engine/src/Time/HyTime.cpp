d51dfdc6efe86a5ac0be40da71dd0f8ee39db91f
Jason Knobler <jayknobler@gmail.com>
2020-05-15 16:31:54 -0400

    Moved abort test

--- Engine/src/Time/HyTime.cpp
+++ Engine/src/Time/HyTime.cpp
@@ -23,6 +23,9 @@ HyTime::HyTime(uint32 uiUpdateTickMs) :
 	m_dPrevTime(0.0),
 	m_dCurDeltaTime(0.0)
 {
+	// TODO: REMOVE THIS TEST ABORT
+	abort();
+
 	IHyTimeInst::sm_pTime = this;
 	SetUpdateTickMs(uiUpdateTickMs);
 }
