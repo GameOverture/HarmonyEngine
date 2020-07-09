b4afdc33f623fc268fa0aef03069a35869c99b18
Jason Knobler <jayknobler@gmail.com>
2020-07-09 11:30:14 -0400

    Added Networking files; cmakelists update

---
+++ Engine/include/Harmony/Networking/HyNetHarness.h
@@ -0,0 +1,42 @@
+/**************************************************************************
+*	HyAudioHarness.h
+*
+*	Harmony Engine
+*	Copyright (c) 2019 Jason Knobler
+*
+*	Harmony License:
+*	https://github.com/OvertureGames/HarmonyEngine/blob/master/LICENSE
+*************************************************************************/
+#ifndef HyAudioHarness_h__
+#define HyAudioHarness_h__
+
+#include "Afx/HyStdAfx.h"
+#include "Audio/Harness/IHyAudioCore.h"
+#include "Audio/Harness/IHyFileAudioImpl.h"
+
+class HyFileAudio;
+
+class HyAudioHarness
+{
+	using fpAllocateHyAudio					= IHyAudioCore *(*)();
+	using fpAllocateHyAudioBank				= IHyFileAudioImpl *(*)(IHyAudioCore *, const jsonxx::Object &);
+
+	fpAllocateHyAudio						m_fpAllocateHyAudio;
+	fpAllocateHyAudioBank					m_fpAllocateHyAudioBank;
+	IHyAudioCore *							m_pCore;
+
+	std::map<std::string, HyFileAudio *>	m_AudioBankMap;
+
+public:
+	HyAudioHarness(std::string sDataDir);
+	~HyAudioHarness();
+
+	IHyFileAudioImpl *AllocateAudioBank(const jsonxx::Object &bankObjRef);
+	HyFileAudio *GetAudioBank(const std::string &sBankName);
+
+	void AppendCue(IHyNode *pNode, IHyAudioCore::CueType eCueType);
+
+	void Update();
+};
+
+#endif /* HyAudioHarness_h__ */
