bc6067c45d42d59157cb3266e2b68a998ec27320
Jason Knobler <jayknobler@gmail.com>
2021-02-15 17:33:55 -0500

    Removed Hy_ friend functions - now are static member functions of HyEngine

--- Editor/data/_projGen/src/%HY_CLASS%.cpp
+++ Editor/data/_projGen/src/%HY_CLASS%.cpp
@@ -1,14 +1,11 @@
 #include "pch.h"
 #include "%HY_CLASS%.h"
 
-extern HyWindow &Hy_Window();
-extern HyInput &Hy_Input();
-
 %HY_CLASS%::%HY_CLASS%(HarmonyInit &initStruct) :
 	HyEngine(initStruct),
-	m_pCamera(Hy_Window().CreateCamera2d())
+	m_pCamera(HyEngine::Window().CreateCamera2d())
 {
-	Hy_Input().MapBtn(INPUT_ExitGame, HYKEY_Escape);
+	HyEngine::Input().MapBtn(INPUT_ExitGame, HYKEY_Escape);
 }
 
 %HY_CLASS%::~%HY_CLASS%()
@@ -17,5 +14,5 @@ extern HyInput &Hy_Input();
 
 /*virtual*/ bool %HY_CLASS%::OnUpdate() /*override*/
 {
-	return !Hy_Input().IsActionReleased(INPUT_ExitGame);
+	return !HyEngine::Input().IsActionReleased(INPUT_ExitGame);
 }
