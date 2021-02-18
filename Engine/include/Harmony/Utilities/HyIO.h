7fee04b48fedf2c74a4900d16cfcbedd2561fea9
Jason Knobler <jayknobler@gmail.com>
2021-02-17 20:24:06 -0500

    HyIO now can encode and decode URL strings

--- Engine/include/Harmony/Utilities/HyIO.h
+++ Engine/include/Harmony/Utilities/HyIO.h
@@ -11,21 +11,20 @@
 #define HyFileIO_h__
 
 #include "Afx/HyStdAfx.h"
-#include <fstream>
 
 class HyIO
 {
 public:
-	static void ReadTextFile(const char *szFilePath, std::vector<char> &sContentsOut);
-
-	static void WriteTextFile(const char *szFilePath, const char *szContentBuffer);
-
+	static std::string CleanPath(const char *szPath, const char *szExtension, bool bMakeLowercase);
 	static bool FileExists(const std::string &sFilePath);
 
-	static std::string CleanPath(const char *szPath, const char *szExtension, bool bMakeLowercase);
+	static void ReadTextFile(const char *szFilePath, std::vector<char> &sContentsOut);
+	static void WriteTextFile(const char *szFilePath, const char *szContentBuffer);
+
+	static std::string UrlEncode(std::string sString);
+	static std::string UrlDecode(std::string sString);
 
 	static bool SaveImage_DTX5(const char *szFilename, int iWidth, int iHeight, const unsigned char *const pUncompressedPixelData);
-
 	static bool SaveImage_DTX1(const char *szFilename, int iWidth, int iHeight, const unsigned char *const pUncompressedPixelData);
 };
 
--- Engine/src/Utilities/HyIO.cpp
+++ Engine/src/Utilities/HyIO.cpp
@@ -13,66 +13,8 @@
 
 #include "soil2/SOIL2.h"
 
-/*static*/ void HyIO::ReadTextFile(const char *szFilePath, std::vector<char> &contentsOut)
-{
-	//SDL_RWops *pBankFile = SDL_RWFromFile(sFilePath.c_str(), "rb");
-	//if(pBankFile == nullptr)
-	//	return false;
-
-	//Sint64 iBankSize = SDL_RWsize(pBankFile);
-	//m_pBankData = HY_NEW unsigned char[iBankSize + 1];
-
-	//Sint64 iTotalReadObjs = 0, iCurReadObjs = 1;
-	//unsigned char *pFilePtr = m_pBankData;
-	//while(iTotalReadObjs < iBankSize && iCurReadObjs != 0)
-	//{
-	//	iCurReadObjs = SDL_RWread(pBankFile, pFilePtr, 1, (iBankSize - iTotalReadObjs));
-	//	iTotalReadObjs += iCurReadObjs;
-	//	pFilePtr += iCurReadObjs;
-	//}
-	//SDL_RWclose(pBankFile);
-
-	//if(iTotalReadObjs != iBankSize)
-	//{
-	//	HyLogError("Failed to read " << sFilePath);
-	//	delete[] m_pBankData;
-	//	return false;
-	//}
-
-	//m_pBankData[iTotalReadObjs] = '\0';
-	//return true;
-
-	if(szFilePath == nullptr)
-		return;
-
-	std::ifstream infile(szFilePath, std::ios::binary | std::ios::ate);
-	HyAssert(infile, "HyReadTextFile invalid file: " << szFilePath);
-
-	std::streamsize size = infile.tellg();
-	infile.seekg(0, std::ios::beg);
-
-	contentsOut.resize(size + 1);
-	if(infile.read(contentsOut.data(), size))
-		contentsOut[size] = '\0';
-	else {
-		HyLogError("HyIO::ReadTextFile - only " << infile.gcount() << " bytes was read");
-	}
-
-	infile.close();
-}
-
-/*static*/ void HyIO::WriteTextFile(const char *szFilePath, const char *szContentBuffer)
-{
-	std::ofstream outfile(szFilePath);
-	outfile.write(szContentBuffer, strlen(szContentBuffer));
-	outfile.close();
-}
-
-/*static*/ bool HyIO::FileExists(const std::string &sFilePath)
-{
-	std::ifstream infile(sFilePath.c_str());
-	return infile.good();
-}
+#include <fstream>
+#include <iomanip>
 
 /*static*/ std::string HyIO::CleanPath(const char *szPath, const char *szExtension, bool bMakeLowercase)
 {
@@ -136,6 +78,141 @@
 		transform(sPath.begin(), sPath.end(), sPath.begin(), ::tolower);
 
 	return sPath;
+}
+
+/*static*/ bool HyIO::FileExists(const std::string &sFilePath)
+{
+	std::ifstream infile(sFilePath.c_str());
+	return infile.good();
+}
+
+/*static*/ void HyIO::ReadTextFile(const char *szFilePath, std::vector<char> &contentsOut)
+{
+	//SDL_RWops *pBankFile = SDL_RWFromFile(sFilePath.c_str(), "rb");
+	//if(pBankFile == nullptr)
+	//	return false;
+
+	//Sint64 iBankSize = SDL_RWsize(pBankFile);
+	//m_pBankData = HY_NEW unsigned char[iBankSize + 1];
+
+	//Sint64 iTotalReadObjs = 0, iCurReadObjs = 1;
+	//unsigned char *pFilePtr = m_pBankData;
+	//while(iTotalReadObjs < iBankSize && iCurReadObjs != 0)
+	//{
+	//	iCurReadObjs = SDL_RWread(pBankFile, pFilePtr, 1, (iBankSize - iTotalReadObjs));
+	//	iTotalReadObjs += iCurReadObjs;
+	//	pFilePtr += iCurReadObjs;
+	//}
+	//SDL_RWclose(pBankFile);
+
+	//if(iTotalReadObjs != iBankSize)
+	//{
+	//	HyLogError("Failed to read " << sFilePath);
+	//	delete[] m_pBankData;
+	//	return false;
+	//}
+
+	//m_pBankData[iTotalReadObjs] = '\0';
+	//return true;
+
+	if(szFilePath == nullptr)
+		return;
+
+	std::ifstream infile(szFilePath, std::ios::binary | std::ios::ate);
+	HyAssert(infile, "HyReadTextFile invalid file: " << szFilePath);
+
+	std::streamsize size = infile.tellg();
+	infile.seekg(0, std::ios::beg);
+
+	contentsOut.resize(size + 1);
+	if(infile.read(contentsOut.data(), size))
+		contentsOut[size] = '\0';
+	else {
+		HyLogError("HyIO::ReadTextFile - only " << infile.gcount() << " bytes was read");
+	}
+
+	infile.close();
+}
+
+/*static*/ void HyIO::WriteTextFile(const char *szFilePath, const char *szContentBuffer)
+{
+	std::ofstream outfile(szFilePath);
+	outfile.write(szContentBuffer, strlen(szContentBuffer));
+	outfile.close();
+}
+
+/*static*/ std::string HyIO::UrlEncode(std::string sStr)
+{
+	std::ostringstream ssEncoded;
+	ssEncoded.fill('0');
+	ssEncoded << std::hex;
+
+	for(std::string::const_iterator iter = sStr.begin(); iter != sStr.end(); ++iter)
+	{
+		std::string::value_type c = (*iter);
+
+		// Keep alphanumeric and other accepted characters intact
+		if(isalnum(c) || c == '-' || c == '_' || c == '.' || c == '~')
+		{
+			ssEncoded << c;
+			continue;
+		}
+
+		// Any other characters are percent-encoded
+		ssEncoded << std::uppercase;
+		ssEncoded << '%' << std::setw(2) << int((unsigned char)c);
+		ssEncoded << std::nouppercase;
+	}
+
+	return ssEncoded.str();
+}
+
+/*static*/ std::string HyIO::UrlDecode(std::string sStr)
+{
+	std::ostringstream ssDecoded;
+
+	for(std::string::const_iterator iter = sStr.begin(); iter != sStr.end(); ++iter)
+	{
+		std::string::value_type c0 = (*iter);
+		
+		if(c0 == '%')
+		{
+			++iter;
+			if(iter == sStr.end())
+			{
+				HyLogWarning("HyIO::UrlDecode parsed invalid '%' encoding");
+
+				ssDecoded << c0;
+				return ssDecoded.str();
+			}
+			std::string::value_type c1 = (*iter);
+
+			++iter;
+			if(iter == sStr.end())
+			{
+				HyLogWarning("HyIO::UrlDecode parsed invalid '%' encoding");
+
+				ssDecoded << c0;
+				--iter; // process whatever 'c1' is at least
+				continue;
+			}
+			std::string::value_type c2 = (*iter);
+
+			std::string sHex;
+			sHex += c1;
+			sHex += c2;
+			int32 iDec = 0;
+			std::istringstream(sHex) >> std::hex >> iDec;
+
+			ssDecoded << static_cast<char>(iDec);
+		}
+		else if(c0 == '+')
+			ssDecoded << ' ';
+		else
+			ssDecoded << c0;
+	}
+
+	return ssDecoded.str();
 }
 
 /*static*/ bool HyIO::SaveImage_DTX5(const char *szFilename, int iWidth, int iHeight, const unsigned char *const pUncompressedPixelData)
