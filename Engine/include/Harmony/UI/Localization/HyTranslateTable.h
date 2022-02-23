e1904d711e38b3fb86b68e51111040d20ce6f69c
Joseph Aurili <49530406+JLaurili@users.noreply.github.com>
2022-02-23 11:25:05 -0700

    Implemented translations

--- Engine/include/Harmony/UI/Localization/HyTranslateTable.h
+++ Engine/include/Harmony/UI/Localization/HyTranslateTable.h
@@ -13,17 +13,23 @@
 #include "Afx/HyStdAfx.h"
 #include <unordered_map>
 
+#define HYTRANSLATETABLE_DataFile "Translations.data"
+
 #define HyTr(x) HyTranslateTable::Get(x)
 
 class HyTranslateTable
 {
-	static std::unordered_map <std::string, std::string>table;
+	static std::unordered_map<std::string, std::string> sm_table;
+	static std::string sm_language;
+	static bool sm_testMode;
 
 public:
 
-	static void Load();
+	static void Load(std::string sIso639Code, std::string sIso3166Code);
 
 	static const std::string Get(const std::string id);
+
+	static void SetTestMode(bool state);
 
 private:
 };
