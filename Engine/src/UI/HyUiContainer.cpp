c622956d054a7ef41473714b659d49d5fb642d31
Jason Knobler <jayknobler@gmail.com>
2022-06-17 18:38:00 -0400

    Fixed UI containers' scrollbars taking input when they're not enabled

--- Engine/src/UI/HyUiContainer.cpp
+++ Engine/src/UI/HyUiContainer.cpp
@@ -37,8 +37,6 @@ HyUiContainer::HyUiContainer(HyOrientation eRootLayoutDirection, const HyPanelIn
 	sm_pContainerList.push_back(this);
 
 	// Scroll bars
-	m_VertBar.SetOnScrollCallback(OnScroll, this);
-	m_HorzBar.SetOnScrollCallback(OnScroll, this);
 	SetScrollBarColor(m_Panel.GetPanelColor());
 	EnableScrollBars(m_bUseVertBar, m_bUseHorzBar);
 
@@ -392,6 +390,9 @@ void HyUiContainer::EnableScrollBars(bool bUseVert, bool bUseHorz)
 	m_bUseVertBar = bUseVert;
 	m_bUseHorzBar = bUseHorz;
 
+	m_VertBar.SetOnScrollCallback(m_bUseVertBar ? OnScroll : nullptr, this);
+	m_HorzBar.SetOnScrollCallback(m_bUseHorzBar ? OnScroll : nullptr, this);
+
 	// Use alpha because *this container may want to SetVisible() and it should then propagate to the scroll bars
 	m_VertBar.alpha.Set(static_cast<int32>(m_bUseVertBar) * 1.0f);
 	m_HorzBar.alpha.Set(static_cast<int32>(m_bUseHorzBar) * 1.0f);
@@ -480,35 +481,36 @@ void HyUiContainer::OnRootLayoutUpdate()
 	int32 iNewHeight = static_cast<int32>(m_Panel.size.Y());
 
 	int32 iScissorMargin = 0;
-	if(m_Panel.IsPrimitive())
-		iScissorMargin = m_Panel.GetFrameSize();
-
 	if(m_bUseVertBar || m_bUseHorzBar)
 	{
+		if(m_Panel.IsPrimitive())
+			iScissorMargin = m_Panel.GetFrameSize();
+
 		SetScissor(iScissorMargin, iScissorMargin,
 			iNewWidth - (iScissorMargin * 2) - (static_cast<int32>(m_bUseVertBar) * m_VertBar.GetDiameter()),
 			iNewHeight - (iScissorMargin * 2) - (static_cast<int32>(m_bUseHorzBar) * m_HorzBar.GetDiameter()));
-	}
-	m_Panel.ClearScissor(false);
-	m_VertBar.ClearScissor(false);
-	m_HorzBar.ClearScissor(false);
 
-	// If scrolling, then use '0' for that dimension, to indicate to the layout use the exact amount it needs.
-	// NOTE: Using the layout's size hint (instead of '0') may be inaccurate if other dimension is being resized
-	if(m_bUseHorzBar)
-		iNewWidth = 0;
-	else if(m_bUseVertBar)
-	{
-		iNewWidth -= (m_RootLayout.GetMargins().left + m_RootLayout.GetMargins().right);
-		iNewWidth -= m_VertBar.GetDiameter();
-	}
+		m_Panel.ClearScissor(false);
+		m_VertBar.ClearScissor(false);
+		m_HorzBar.ClearScissor(false);
 
-	if(m_bUseVertBar)
-		iNewHeight = 0;
-	else if(m_bUseHorzBar)
-	{
-		iNewHeight -= (m_RootLayout.GetMargins().top + m_RootLayout.GetMargins().bottom);
-		iNewHeight -= m_HorzBar.GetDiameter();
+		// If scrolling, then use '0' for that dimension, to indicate to the layout use the exact amount it needs.
+		// NOTE: Using the layout's size hint (instead of '0') may be inaccurate if other dimension is being resized
+		if(m_bUseHorzBar)
+			iNewWidth = 0;
+		else if(m_bUseVertBar)
+		{
+			iNewWidth -= (m_RootLayout.GetMargins().left + m_RootLayout.GetMargins().right);
+			iNewWidth -= m_VertBar.GetDiameter();
+		}
+
+		if(m_bUseVertBar)
+			iNewHeight = 0;
+		else if(m_bUseHorzBar)
+		{
+			iNewHeight -= (m_RootLayout.GetMargins().top + m_RootLayout.GetMargins().bottom);
+			iNewHeight -= m_HorzBar.GetDiameter();
+		}
 	}
 
 	glm::ivec2 vActualSize = m_RootLayout.Resize(iNewWidth, iNewHeight);
