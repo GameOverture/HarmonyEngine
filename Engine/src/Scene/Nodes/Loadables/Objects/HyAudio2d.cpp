f99507f216a1a4f3d7e1afd9bd0e27e7aaf779a8
Jason Knobler <jayknobler@gmail.com>
2023-12-24 17:48:36 -0500

    Fixed Auxiliary audio Init() functions

--- Engine/src/Scene/Nodes/Loadables/Objects/HyAudio2d.cpp
+++ Engine/src/Scene/Nodes/Loadables/Objects/HyAudio2d.cpp
@@ -38,18 +38,21 @@ HyAudio2d::HyAudio2d(std::string sFilePath, bool bIsStreaming, int32 iInstanceLi
 
 void HyAudio2d::Init(uint32 uiAudioChecksum, uint32 uiBankId, HyEntity2d *pParent)
 {
+	IHyNode::sm_pScene->ProcessAudioCue(this, HYSOUNDCUE_Stop);
 	m_uiFlags |= SETTING_IsAuxiliary;
 	IHyLoadable2d::Init(std::to_string(uiAudioChecksum), std::to_string(uiBankId), pParent);
 }
 
 void HyAudio2d::Init(HyAudioHandle hAudioHandle, HyEntity2d *pParent)
 {
+	IHyNode::sm_pScene->ProcessAudioCue(this, HYSOUNDCUE_Stop);
 	m_uiFlags |= SETTING_IsAuxiliary;
 	IHyLoadable2d::Init(std::to_string(hAudioHandle.first), std::to_string(hAudioHandle.second), pParent);
 }
 
 void HyAudio2d::Init(std::string sFilePath, bool bIsStreaming, int32 iInstanceLimit, int32 iCategoryId, HyEntity2d *pParent)
 {
+	IHyNode::sm_pScene->ProcessAudioCue(this, HYSOUNDCUE_Stop);
 	m_uiFlags |= SETTING_IsAuxiliary;
 	HyAudioHandle hAudioHandle = HyEngine::CreateAudio(HyIO::CleanPath(sFilePath.c_str()), bIsStreaming, iInstanceLimit, iCategoryId);
 	IHyLoadable2d::Init(std::to_string(hAudioHandle.first), std::to_string(hAudioHandle.second), pParent);
